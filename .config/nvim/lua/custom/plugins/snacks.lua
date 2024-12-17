858dc4083e5500913f89bdead82db65cf6390059
spoonmilk <haku31229@gmail.com>
2024-12-16 19:24:30 -0500

    smoooooth

--- .config/nvim/lua/custom/plugins/snacks.lua
+++ .config/nvim/lua/custom/plugins/snacks.lua
@@ -14,13 +14,12 @@ return {
     dim = { enabled = true },
     zen = { enabled = true },
     scroll = { enabled = true },
-    -- dashboard = {
-
-    --   sections = {
-    --     { section = 'header' },
-    --     { section = 'keys', gap = 1, padding = 1 },
-    --     { section = 'startup' },
-    --   },
-    -- },
+    dashboard = {
+      sections = {
+        { section = 'header' },
+        { section = 'keys', gap = 1, padding = 1 },
+        { section = 'startup' },
+      },
+    },
   },
 }
