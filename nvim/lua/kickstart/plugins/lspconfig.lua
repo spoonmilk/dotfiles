c4c0ce5a55fc69dbae7fe9cd7746d758d67f811e
spoonmilk <haku31229@gmail.com>
2025-03-01 14:35:02 -0500

    fix: update tmux statusline colors

--- nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/lua/kickstart/plugins/lspconfig.lua
@@ -201,6 +201,7 @@ return {
         },
         rust_analyzer = {},
         texlab = {},
+        julials = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
