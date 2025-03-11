86cb6a0ccd62fb5eef068bf844c92bcbef59b26f
spoonmilk <haku31229@gmail.com>
2025-03-11 01:25:03 -0400

    fix: switch to ccls

--- nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/lua/kickstart/plugins/lspconfig.lua
@@ -179,7 +179,6 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
         gopls = {},
         pylsp = {
           settings = {
@@ -202,15 +201,6 @@ return {
         rust_analyzer = {},
         texlab = {},
         julials = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -226,6 +216,14 @@ return {
           },
         },
         astro = {},
+        ccls = {
+          filetypes = { 'c', 'h' },
+          init_options = {
+            cache = {
+              directory = '~/src/ccls',
+            },
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
@@ -248,6 +246,8 @@ return {
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        ensure_installed = {},
+        automatic_installation = false,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
