406370659826a3574edaec043c0bb44aa09f3e8e
spoonmilk <haku31229@gmail.com>
2025-03-12 20:11:49 -0400

    fix: no copilot, pls

--- nvim/lua/kickstart/plugins/cmp.lua
+++ nvim/lua/kickstart/plugins/cmp.lua
@@ -115,7 +115,6 @@ return {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
-          { name = 'copilot' },
         },
         formatting = {
           format = lspkind.cmp_format {
