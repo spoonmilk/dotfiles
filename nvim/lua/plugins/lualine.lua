8f21f7035611224e3d21c0da2014d215f9aa2198
spoonmilk <haku31229@gmail.com>
2025-05-03 15:06:12 -0400

    feat: reorganize lualine, add black metal

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,6 +4,9 @@ return {
     local git_blame = require 'gitblame'
     -- This disables showing of the blame text next to the cursor
     vim.g.gitblame_display_virtual_text = 0
+    vim.g.gitblame_message_template = '<summary> • <date> • <author>'
+    vim.g.gitblame_message_when_not_committed = 'Commit to the bit!'
+    vim.g.gitblame_date_format = '%r %H'
     require('lualine').setup {
       options = {
         -- theme = "gruvbox",
@@ -13,9 +16,9 @@ return {
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available } },
-        lualine_y = {},
+        lualine_c = { 'filename', { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available } },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
         lualine_z = { 'location' },
       },
     }
